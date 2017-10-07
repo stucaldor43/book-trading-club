d0f96aa89007066f124570f227c05ad02d25119b
Anthony Cook <ddragonmaster49@yahoo.com>
2017-10-07 16:11:58 -0400

    Alter database schema

--- initial_schema.js
+++ initial_schema.js
@@ -1,30 +1,32 @@
 module.exports.up = function(knex) {
   return knex.schema
-    .createTable("address", (table) => {
+    .createTableIfNotExists("address", (table) => {
         table.increments("id").primary();
         table.string("city");
         table.string("state");
     })
-    .createTable("client", (table) => {
+    .createTableIfNotExists("client", (table) => {
         table.increments("id").primary();
         table.string("first_name");
         table.string("last_name");
         table.string("display_name").notNullable().unique("display_name");
         table.integer("fk_address_id").unsigned().references("id").inTable("address");
     })
-    .createTable("book", (table) => {
+    .createTableIfNotExists("book", (table) => {
+        table.increments("id").primary();
+        table.integer("fk_client_id").unsigned().references("id").inTable("client").onDelete("CASCADE").notNullable();
+    })
+    .createTableIfNotExists("book_details", (table) => {
         table.increments("id").primary();
         table.string("author").notNullable();
         table.string("title").notNullable();
         table.string("cover_image_url").notNullable();
-        table.integer("fk_client_id").unsigned().references("id").inTable("client").onDelete("CASCADE").notNullable();
-    })
-    .createTable("offer", (table) => {
         table.integer("fk_book_id").unsigned().references("id").inTable("book").onDelete("CASCADE").notNullable();
-        table.integer("fk_book_id2").unsigned().references("id").inTable("book").onDelete("CASCADE").notNullable();
-        table.integer("fk_client_id").unsigned().references("id").inTable("client").onDelete("CASCADE").notNullable();
-        table.integer("fk_client_id2").unsigned().references("id").inTable("client").onDelete("CASCADE").notNullable();
-        table.primary(["fk_book_id", "fk_book_id2", "fk_client_id", "fk_client_id2"]);
+    })
+    .createTableIfNotExists("offer", (table) => {
+        table.integer("requested_book").unsigned().references("id").inTable("book").onDelete("CASCADE").notNullable();
+        table.integer("offered_book").unsigned().references("id").inTable("book").onDelete("CASCADE").notNullable();
+        table.primary(["requested_book", "offered_book"]);
     })
 };
 
@@ -34,4 +36,5 @@ module.exports.down = function(knex) {
         .dropTableIfExists("book")
         .dropTableIfExists("client")
         .dropTableIfExists("address")
+        .dropTableIfExists("book_details")
 };
