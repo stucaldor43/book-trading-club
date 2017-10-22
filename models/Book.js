00eb5382393bf2a5412a64b3f8ec61cc6d21881f
Anthony Cook <ddragonmaster49@yahoo.com>
2017-10-22 15:05:51 -0400

    Add book model

---
+++ models/Book.js
@@ -0,0 +1,57 @@
+const Model = require("objection").Model;
+
+class Book extends Model {
+    static get tableName() {
+        return "book";
+    }
+
+    static get relationMappings() {
+        const Client = require("./Client");
+        const BookDetails = require("./BookDetails");
+        
+        return {
+            metadata: {
+                relation: Model.HasManyRelation,
+                modelClass: BookDetails,
+                join: {
+                    from: "book.id",
+                    to: "book_details.fk_book_id"
+                }
+            },
+            owner: {
+                relation: Model.BelongsToOneRelation,
+                modelClass: Client,
+                join: {
+                    from: "book.fk_client_id",
+                    to: "client.id"
+                }
+            },
+            offers: {
+                relation: Model.ManyToManyRelation,
+                modelClass: Book,
+                join: {
+                    from: "book.id",
+                    through: {
+                        from: "offer.offered_book",
+                        to: "offer.requested_book"
+                    },
+                    to: "book.id"
+                }
+            },
+            requests: {
+                relation: Model.ManyToManyRelation,
+                modelClass: Book,
+                join: {
+                    from: "book.id",
+                    through: {
+                        from: "offer.requested_book",
+                        to: "offer.offered_book"
+                    },
+                    to: "book.id"
+                }
+            }
+        }
+    }
+}
+
+module.exports = Book;
