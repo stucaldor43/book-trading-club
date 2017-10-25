e773b68a79ec12acd54baf017d9973c3b1ebabef
Anthony Cook <ddragonmaster49@yahoo.com>
2017-10-25 13:23:26 -0400

    Use the same server address regardless of environment

--- config.js
+++ config.js
@@ -1,16 +1,8 @@
 let config = {};
-switch(process.env.NODE_ENV) {
-    case "dev": 
-        config = {
-            protocol: "http",
-            domain: "localhost",
-            port: 8080
-        }
-        break;
-    case "production": 
-        break;
-    default:
-        throw new Error("Illegal environment value");
-}
+config = {
+  protocol: "http",
+  domain: "localhost",
+  port: 8080
+};
 
 module.exports = config;
