731c85481d09262c623643da94cc95ca3ea58354
Anthony Cook <ddragonmaster49@yahoo.com>
2017-12-19 15:44:48 -0500

    Add routes

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,3 +1,29 @@
 const router = require("express").Router();
+const Book = require("./../../models/Book");
+const jsonparser = require("body-parser").json();
+
+router.get("/:id", async (req, res) => {
+    const book = await Book
+                        .query()
+                        .findById(req.params.id)
+    if (!book) {
+    res.sendStatus(400)
+    }
+    res.sendStatus(204);
+});
+
+router.post("/", async (req, res) => {
+
+});
+
+router.delete("/:id", async (req, res) => {
+    const book = await Book
+                        .query()
+                        .deleteById(req.params.id)
+    if (!book) {
+        res.sendStatus(400)
+    }
+    res.sendStatus(204);
+});
 
 module.exports = router;
--- routes/api/clients.js
+++ routes/api/clients.js
@@ -1,7 +1,40 @@
 const router = require("express").Router();
+const Client = require("./../../models/Client");
+const { jsend } = require("./../../helpers/utils.js");
 
-router.get("/", (req, res) => {
-    res.send(req.session.username);
+router.get("/:id", async (req, res) => {
+    const client = await Client
+                            .query()
+                            .findById(req.params.id);
+    if (!client) {
+        return res.sendStatus(400);
+    }
+    res.json(jsend.success({client}));
+});
+
+router.post("/", async (req, res) => {
+    const clientsWithSameName = await Client
+                                        .query()
+                                        .where("display_name", req.session.username || req.ip);
+    if (clientsWithSameName.length > 0) {
+        return res.sendStatus(400);
+    }
+    const newUser = await Client
+                            .query()
+                            .insert({display_name: req.session.username || req.ip});
+    res.json(jsend.success({client: newUser}));
+});
+
+router.delete("/:id", async (req, res) => {
+    const user = await Client
+                        .query()
+                        .where("display_name", req.session.username || req.ip)
+                        .first();
+    if (user.id !== Number(req.params.id)) {
+        res.sendStatus(400);
+    }
+    await Client.query().deleteById(user.id);
+    res.sendStatus(204);
 });
 
 module.exports = router;
