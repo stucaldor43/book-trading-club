11f980ce2a83be031b2cbebbf1fb2c84e7011294
Anthony Cook <ddragonmaster49@yahoo.com>
2018-01-13 12:26:49 -0500

    Allow user to set their name and location

--- components/UserSettings.jsx
+++ components/UserSettings.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 class UserSettings extends React.Component {
     constructor(props) {
@@ -7,20 +7,36 @@ class UserSettings extends React.Component {
             user: {
                 name: "Stu Pickles",
                 city: "Yucaipa",
-                state: "California"
             }
         }
+        this.renderStateOptions = this.renderStateOptions.bind(this);
+        this.saveUserInformation = this.saveUserInformation.bind(this);
+    }
+
+    renderStateOptions() {
+        return (
+            <select className="userSettings-stateDropDown" name="state">
+                <option value="NY">NY</option>
+                <option value="CA">CA</option>
+                <option value="MA">MA</option>
+            </select>
+        );
+    }
+
+    saveUserInformation() {
+        // send user data to server
+        alert("information saved!");
     }
 
     render() {
         return (
-            <div>
-                <form>
-                    <button onClick={(evt) => {
-                        console.log(evt)
-                    }}>Save</button>
-                    <button>Discard Changes</button>
-                </form>
+            <div className="page userSettings">
+                <ul>
+                    <li><input className="userSettings-nameInput" type="text" placeholder="Full Name" onKeyUp={ this.keyUpHandler } value={this.state.user.name}/></li>
+                    <li><input className="userSettings-cityInput" type="text" placeholder="City" onKeyUp={ this.keyUpHandler } value={this.state.user.city}/></li>
+                    <li>{ this.renderStateOptions() }</li>
+                    <li><button className="userSettings-saveChangesButton" onClick={this.saveUserInformation}>Save</button></li>
+                </ul>
             </div>
         );
     }
