f028eb1febdfa76433557a77688686162007603e
Anthony Cook <ddragonmaster49@yahoo.com>
2018-02-04 13:28:10 -0500

    Add headers

--- routes/api/index.js
+++ routes/api/index.js
@@ -6,7 +6,8 @@ const allowOrigin = (req, res, next) => {
     res.set({
         "Access-Control-Allow-Origin": "http://localhost:8081",
         "Access-Control-Allow-Credentials": true,
-        "Access-Control-Allow-Headers": "Content-Type"
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "GET, POST, PATCH, DELETE"
     });
     next();
 };
