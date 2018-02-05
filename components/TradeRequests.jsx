4b45dd2c40168704836d9a235c8108caf4c5640f
Anthony Cook <ddragonmaster49@yahoo.com>
2018-02-05 10:35:01 -0500

    Allow user to trade books

--- components/TradeRequests.jsx
+++ components/TradeRequests.jsx
@@ -1,14 +1,86 @@
 import React from "react";
+import BookOfferDialog from './BookOfferDialog';
+import Pagination from './Pagination.jsx';
+import OfferProposalWindow from './OfferProposalWindow';
 
 class TradeRequests extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            offers: []
+        }
+        this.tradeBooks = this.tradeBooks.bind(this);
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:8080/api/offer/requests_received', {
+            credentials: 'include'
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            this.setState((prevState, props) => {
+                return ({
+                    offers: [...prevState.offers, ...data.offers]
+                })
+            })
+        });
+    }
+
+    tradeBooks(offer) {
+        const tradeData = {
+            tradeProposerName: offer.offered_book_owner,
+            tradeConfirmerBookId: offer.requested_book_id,
+            tradeProposerBookId: offer.offered_book_id,
+        }
+        fetch('http://localhost:8080/api/book/trade', {
+            method: "POST",
+            body: JSON.stringify(tradeData),
+            credentials: 'include'
+        })
+        .then((data) => {
+            alert("trade completed!");
+        });
     }
 
     render() {
         return (
-            <div>
-
+            <div className="page">
+                <div className="tableContainer">
+                    <table>
+                        <caption>Requests Received</caption>
+                        <tr>
+                            <th>Offered Book</th>
+                            <th>Your Book</th>
+                            <th>Username</th>
+                            <th>Accept Trade</th>
+                        </tr>
+                        { this.state.offers.map((offer) => {
+                            return (
+                                <tr>
+                                    <td>{offer.offered_book_title}</td>
+                                    <td>{offer.requested_book_title}</td>
+                                    <td>{offer.offered_book_owner}</td>
+                                    <td><button onClick={this.tradeBooks.bind(offer)}>Confirm</button></td>
+                                </tr>
+                            )
+                        })}
+                        <tr>
+                            <td>The Search for Red October</td>
+                            <td>Harry Potter and the Sorceror's Stone</td>
+                            <td>SamsonTheDestroyer84</td>
+                            <td><button>Confirm</button></td>
+                        </tr>
+                        <tr>
+                            <td>Lord of the Rings</td>
+                            <td>Star Trek: The Next Generation</td>
+                            <td>BickleBartThe42nd</td>
+                            <td><button>Confirm</button></td>
+                        </tr>
+                    </table>
+                </div>
+                <BookOfferDialog/>
+                <OfferProposalWindow/>
+                {/* <Pagination page={2} maxPages={3} url={'http://localhost:8080'}/> */}
             </div>
         );
     }
