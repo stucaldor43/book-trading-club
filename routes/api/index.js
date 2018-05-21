596da9bb8ab2a84f4c50135b1c9877211a58a014
Anthony Cook <ddragonmaster49@yahoo.com>
2018-05-21 11:38:54 -0400

    Add more code to aid in debugging

--- routes/api/index.js
+++ routes/api/index.js
@@ -40,8 +40,8 @@ router.get("/get_access_token", (req, res) => {
             
     const {secret, token, verifier} = req.query;
     oauth.getOAuthAccessToken(token, secret, verifier, function(err, access_token, access_secret, user) {
-        if (err) {
-            console.log(err, accces_token, access_secret, user);
+        if (!user) {
+            console.log(user);
             res.json({
                 status: "fail", 
                 data: {}
